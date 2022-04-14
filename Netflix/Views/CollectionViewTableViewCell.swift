27873f9c65a64f799bbc474bf541ab6eddea9ec3
denisLebedinsky <den.lebedinsky@gmail.com>
2022-04-15 00:43:02 +0300

    add core data with manager

--- Netflix/Views/CollectionViewTableViewCell.swift
+++ Netflix/Views/CollectionViewTableViewCell.swift
@@ -53,6 +53,17 @@ class CollectionViewTableViewCell: UITableViewCell {
             self?.collectionView.reloadData()
         }
     }
+    
+    private func downloadTitleAt(indexPath: IndexPath){
+        DataPersistenceManager.shared.downloadTitleWith(model: titles[indexPath.row]) { result in
+            switch result {
+            case .success():
+                NotificationCenter.default.post(Notification(name:  Notification.Name("Downloaded"), object: nil))
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
 }
 
 extension CollectionViewTableViewCell: UICollectionViewDataSource, UICollectionViewDelegate {
@@ -99,5 +110,19 @@ extension CollectionViewTableViewCell: UICollectionViewDataSource, UICollectionV
                 print(error.localizedDescription)
             }
         }
+    }
+    
+    //context menu (bluring when long tapped on item)
+    func collectionView(_ collectionView: UICollectionView, contextMenuConfigurationForItemAt indexPath: IndexPath, point: CGPoint) -> UIContextMenuConfiguration? {
+        let config = UIContextMenuConfiguration(
+            identifier: nil,
+            previewProvider: nil) { [weak self] _ in
+                let downloadAction = UIAction(title: "Download", subtitle: nil, image: nil, identifier: nil, discoverabilityTitle: nil, state: .off) { _ in
+                    self?.downloadTitleAt(indexPath: indexPath)
+                }
+                
+                return UIMenu(title: "", image: nil, identifier: nil, options: .displayInline, children: [downloadAction])
+            }
+        return config
     }
 }
